450bac09f9cb756b23d909214e7c5bc86edcfa90
曹弘扬 <296786475@qq.com>
2019-02-02 08:46:48 +0800

    Initial Commit

---
+++ Adonis_20190201_OpenGL_ES/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Adonis_20190201_OpenGL_ES
+//
+//  Created by Adonis_HongYang on 2019/2/2.
+//  Copyright © 2019年 Nikoyo (China）Electronics Systems Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Adonis_20190201_OpenGL_ES/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Adonis_20190201_OpenGL_ES
+//
+//  Created by Adonis_HongYang on 2019/2/2.
+//  Copyright © 2019年 Nikoyo (China）Electronics Systems Co., Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
